fc0472b2196452ee64368708b34ad650921bf3c6
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2020-02-20 14:01:51 -0500

    Add SignInForm

---
+++ src/auth/SignInForm.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useState } from "react";
+
+function SignInForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="w-full max-w-xs">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            for="username"
+          >
+            Username
+          </label>
+          <input
+            className="form-input shadow w-full py-2 px-3 text-gray-700 leading-tight"
+            id="username"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            for="password"
+          >
+            Password
+          </label>
+          <input
+            className="form-input shadow w-full py-2 px-3 text-gray-700 mb-3 leading-tight"
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Sign In
+          </button>
+          <button className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+            Forgot Password?
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SignInForm;
--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import SignInForm from "../auth/SignInForm";
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="bg-gray-100">
+      <div className="flex items-center justify-center min-h-screen">
+        <SignInForm />
+      </div>
+    </div>
+  );
 }
 
 export default App;
