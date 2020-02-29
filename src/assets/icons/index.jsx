1e1428c063a059614e6e3345368b011fc6771586
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2020-02-29 17:34:13 -0500

    Add app assets

---
+++ src/assets/icons/index.jsx
@@ -0,0 +1,6 @@
+export { ReactComponent as FilterIcon } from "./filter.svg";
+export { ReactComponent as MenuIcon } from "./menu.svg";
+export { ReactComponent as SearchIcon } from "./search.svg";
+export { ReactComponent as StarIcon } from "./star.svg";
+export { ReactComponent as StarBorderIcon } from "./star-border.svg";
+export { ReactComponent as StarHalfIcon } from "./star-half.svg";
